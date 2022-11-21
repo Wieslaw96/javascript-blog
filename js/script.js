8af02d0bb626ab41070183172f8304cdf8a4a498
Wieslaw Kaminski <w.kaminski@vp.pl>
2022-11-21 23:39:57 +0100

    javascript-blog

--- js/script.js
+++ js/script.js
@@ -34,40 +34,51 @@
 		targetArticle.classList.add("active");
 	};
 
-	const links = document.querySelectorAll(".titles a");
+	/*const links = document.querySelectorAll(".titles a");
+	
 
 	for (let link of links) {
 		link.addEventListener("click", titleClickHandler);
-	}
+	}*/
 	const optArticleSelector = ".post",
 		optTitleSelector = ".post-title",
 		optTitleListSelector = ".titles";
 
 	function generateTitleLinks() {
 		/* remove contents of titleList */
-		const titleList = (document.querySelector(optTitleListSelector).innerHTML =
-			"");
+		const titleList = document.querySelector(optTitleListSelector);
+		titleList.innerHTML = "";
 
 		/* for each article */
 
 		const articles = document.querySelectorAll(optArticleSelector);
-
+		let html = "";
 		for (let article of articles) {
+			/* get the article id */
+			const articleId = article.getAttribute("id");
+
+			/* find the title element */
+
+			/* get the title from the title element */
+			const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+			/* create HTML of the link */
+			const linkHTML =
+				'<li><a href="#' +
+				articleId +
+				'"><span>' +
+				articleTitle +
+				"</span></a></li>";
+
+			/* insert link into titleList */
+			html = html + linkHTML;
 		}
+		titleList.innerHTML = html;
+		const links = document.querySelectorAll(".titles a");
 
-		/* get the article id */
-        const articleId = optArticleSelector.getAttribute("Id");
-
-		/* find the title element */
-
-		/* get the title from the title element */
-        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-
-		/* create HTML of the link */
-        const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
-        console.log("linkHTML");
-
-		/* insert link into titleList */
+		for (let link of links) {
+			link.addEventListener("click", titleClickHandler);
+		}
 	}
 
 	generateTitleLinks();
