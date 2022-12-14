97bcb84c94adc6258809d97f3a0aaef0fe644e22
Wieslaw Kaminski <w.kaminski@vp.pl>
2022-12-14 19:53:37 +0100

    javascript-blog

--- js/script.js
+++ js/script.js
@@ -133,7 +133,16 @@ function generateTags() {
 	/* [NEW] START LOOP: for each tag in allTags: */
 	for (let tag in allTags) {
 		/* [NEW] generate code of a link and add it to allTagsHTML */
-		allTagsHTML += tag + " (" + allTags[tag] + ") ";
+		//	allTagsHTML += tag + " (" + allTags[tag] + ") ";
+		allTagsHTML +=
+			'<li><a href="#tag-' +
+			tag +
+			'">' +
+			tag +
+			" (" +
+			allTags[tag] +
+			") " +
+			"</a></li>";
 	}
 	/* [NEW] END LOOP: for each tag in allTags: */
 
@@ -183,7 +192,7 @@ function tagClickHandler(event) {
 function addClickListenersToTags() {
 	/* find all links to tags */
 	const allTagLinks = ".post-tags .list a";
-	const allLinks = document.querySelectorAll(allTagLinks);
+	const allLinks = document.querySelectorAll([allTagLinks, ".list.tags a"]);
 
 	/* START LOOP: for each link */
 	for (let eachLink of allLinks) {
